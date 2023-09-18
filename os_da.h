88b3be183a8728abfc19c956ce7ff5153d367c5b
Oran Simhony <oran.simhony@gmail.com>
2023-09-18 16:44:28 +0300

    Make dynamic array more comfortable to use

--- os_da.h
+++ os_da.h
@@ -20,51 +20,76 @@
 #define OSDA_FACTOR (2)
 #endif
 
+/*
+Dynamic Array Type template:
 typedef struct
 {
     void *items;
     size_t count;
     size_t capacity;
 } os_dynamic_array_t;
+ */
 
 #ifndef OSDA_REALLOC
 #define OSDA_REALLOC realloc
 #endif
 
-OSDADEF os_dynamic_array_t os_da_new(void);
-OSDADEF os_dynamic_array_t os_da_pop_tail(os_dynamic_array_t da);
-OSDADEF os_dynamic_array_t os_da_free(os_dynamic_array_t da);
+#ifndef OSDA_FREE
+#define OSDA_FREE free
+#endif
 
-static void *da__realloc(void *ptr, size_t size);
-
-#define os_da_append(type, da, item)                                           \
+#define os_da_free(da)                                                         \
     do                                                                         \
     {                                                                          \
-        if (da.count + 1 >= da.capacity)                                       \
+        if ((da).items)                                                        \
         {                                                                      \
-            size_t new_capacity = da.capacity == 0                             \
-                                    ? OSDA_INITIAL_CAPACITY                    \
-                                    : da.capacity * OSDA_FACTOR;               \
-            da.items = da__realloc(da.items, new_capacity);                    \
-            da.capacity = new_capacity;                                        \
+            da__free((da).items);                                              \
+            (da).items = NULL;                                                 \
         }                                                                      \
-        memcpy((type *) da.items + da.count, &(type) {item}, sizeof(type));    \
-        ++da.count;                                                            \
+        (da).count = 0;                                                        \
+        (da).capacity = 0;                                                     \
     } while (0)
 
-#define os_da_get(type, da, index) ((type *)(da).items)[(index)]
+static void *da__realloc(void *ptr, size_t size);
+static void da__free(void *ptr);
+
+#define os_da_append(da, item)                                                 \
+    do                                                                         \
+    {                                                                          \
+        if ((da).count >= (da).capacity)                                       \
+        {                                                                      \
+            size_t new_capacity = (da).capacity == 0                           \
+                                    ? OSDA_INITIAL_CAPACITY                    \
+                                    : (da).capacity * OSDA_FACTOR;             \
+            while ((da).count >= new_capacity)                                 \
+            {                                                                  \
+                (da).capacity *= OSDA_FACTOR;                                  \
+            }                                                                  \
+            (da).items                                                         \
+                = da__realloc((da).items, new_capacity * sizeof(*(da).items)); \
+            (da).capacity = new_capacity;                                      \
+        }                                                                      \
+        (da).items[(da).count++] = item;                                       \
+    } while (0)
+
+#define os_da_pop_tail(da)                                                     \
+    do                                                                         \
+    {                                                                          \
+        if ((da).count > 0)                                                    \
+            --(da).count;                                                      \
+    } while (0)
 
 #define os_da_pop_head(type, da)                                               \
     do                                                                         \
     {                                                                          \
-        if (da.count <= 1)                                                     \
+        if ((da).count <= 1)                                                   \
         {                                                                      \
-            da = os_da_pop_tail(da);                                           \
+            os_da_pop_tail((da));                                              \
         }                                                                      \
         else                                                                   \
         {                                                                      \
-            memmove(da.items, (type *) da.items + 1, sizeof(type));            \
-            --da.count;                                                        \
+            memmove((da).items, (type *) (da).items + 1, sizeof(type));        \
+            --(da).count;                                                      \
         }                                                                      \
     } while (0)
 
@@ -77,34 +102,9 @@ static void *da__realloc(void *ptr, size_t size)
     return OSDA_REALLOC(ptr, size);
 }
 
-OSDADEF os_dynamic_array_t os_da_new(void)
+static void da__free(void *ptr)
 {
-    return (os_dynamic_array_t) {
-        .items = NULL,
-        .count = 0,
-        .capacity = 0,
-    };
-}
-
-OSDADEF os_dynamic_array_t os_da_pop_tail(os_dynamic_array_t da)
-{
-    if (da.count > 0)
-        --da.count;
-
-    return da;
-}
-
-OSDADEF os_dynamic_array_t os_da_free(os_dynamic_array_t da)
-{
-    if (da.items)
-    {
-        da__realloc(da.items, 0);
-        da.items = NULL;
-    }
-
-    da.count = 0;
-    da.capacity = 0;
-    return da;
+    OSDA_FREE(ptr);
 }
 
 #endif
